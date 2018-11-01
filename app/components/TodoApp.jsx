1e2cc354fc9dbe702bc31fd09e986d694c456904
Rahul Kumar <rahul@Rahuls-MacBook-Pro.local>
2018-11-01 12:23:05 +0530

    add toggle todo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -12,11 +12,13 @@ var TodoApp = React.createClass({
             todos: [
                 {
                     id: uuid(),
-                    text: 'walk the dog'
+                    text: 'walk the dog',
+                    completed: false
                 },
                 {
                     id: uuid(),
-                    text: 'clean the yard'
+                    text: 'clean the yard',
+                    completed: true
                 }    
             ]
         };
@@ -24,28 +26,41 @@ var TodoApp = React.createClass({
     handleAddTodo: function (todo) {
         var newTodo = {
             id: uuid(),
-            text: todo
+            text: todo,
+            completed: false
         }
         this.setState({
             todos: [
                 ...this.state.todos,
                 newTodo
             ]
-        })
+        });
         //alert(todo);
     },
     handleSearch: function (showCompleted, searchText) {
         this.setState({
             showCompleted: showCompleted,
             searchText: searchText.toLowerCase()
-        })
+        });
+    },
+    handleToggle: function (id) {
+        var updatedTodos = this.state.todos.map((todo) => {
+            if(id === todo.id) {
+                todo.completed = !todo.completed;
+            }
+            return todo;
+        });
+
+        this.setState({
+            todos: updatedTodos
+        });
     },
     render: function () {
         var {todos} = this.state;
         return (
             <div className = "column small-centered medium-6 large-4 cont">
                 <TodoSearch onSearch = {this.handleSearch}/>
-                <TodoList todos = {todos} />
+                <TodoList todos = {todos} onToggle = {this.handleToggle}/>
                 <AddTodo onAddTodo = {this.handleAddTodo} />
             </div>
         )
