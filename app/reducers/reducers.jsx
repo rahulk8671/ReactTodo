6e80115ad5472283dba334e723d84ef14edd8f58
Rahul Kumar <rahul@Rahuls-MacBook-Pro.local>
2018-11-13 16:07:32 +0530

    resloved

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,4 +1,8 @@
 
+var uuid = require('node-uuid');
+var moment = require('moment');
+
+
 export var searchTextReducer = (state = '', action) => {
     switch (action.type) {
         case 'SET_SEARCH_TEXT':
@@ -17,3 +21,38 @@ export var showCompletedReducer = (state = false, action) => {
     }
 };
 
+export var todosReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state,
+                {
+                    id: uuid(),
+                    text: action.text,
+                    completed: false,
+                    createdAt: moment().unix(),
+                    completedAt: undefined
+                }
+            ];
+        case 'TOGGLE_TODO':
+            return state.map((todo) => {
+                if(action.id === todo.id) {
+                    var nextCompleted = !todo.completed;
+                    var c1 = {
+                        completed: nextCompleted
+                    }
+                    var c2 = {
+                        completedAt: nextCompleted ? moment().unix() : undefined
+                    }
+                    return Object.assign({}, todo, {completed: nextCompleted, completedAt: nextCompleted ? moment().unix() : undefined});//{
+                        // ...todo,
+                        // completed: nextCompleted,
+                        // completedAt: nextCompleted ? moment().unix() : undefined
+                        
+                    //};
+                }
+            })
+        default:
+            return state;
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
             '$': 'jquery',
             'jQuery': 'jquery'
         })
+        
     ],
     output: {
         path: __dirname,
