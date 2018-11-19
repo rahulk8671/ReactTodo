f8f16561304f729564be9be134a604db8ef8c49e
Rahul Kumar <rahul@Rahuls-MacBook-Pro.local>
2018-11-19 17:09:50 +0530

    ahhh

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -1,21 +1,29 @@
 var React = require('react');
+var {connect} = require('react-redux');
+var actions = require('actions');
 
-var TodoSearch = React.createClass({
-    handleSearch: function () {
-        var showCompleted = this.refs.showCompleted.checked;
-        var searchText = this.refs.searchText.value;
+export var TodoSearch = React.createClass({
+    // handleSearch: function () {
+    //     var showCompleted = this.refs.showCompleted.checked;
+    //     var searchText = this.refs.searchText.value;
 
-        this.props.onSearch(showCompleted, searchText);
-    },
+    //     this.props.onSearch(showCompleted, searchText);
+    // },
     render: function () {
+        var {dispatch, showCompleted, searchText} = this.props;
         return (
             <div className = "container_header">
                 <div>
-                    <input type="search" ref="searchText" placeholder="Search Todos" onChange={this.handleSearch}/>
+                    <input type="search" ref="searchText" placeholder="Search Todos" value={searchText} onChange={() => {
+                        var searchText = this.refs.searchText.value;
+                        dispatch(actions.setSearchText(searchText));
+                    }}/>
                 </div>
                 <div>
                     <label>
-                        <input type="checkbox" ref="showCompleted" onChange={this.handleSearch}/>
+                        <input type="checkbox" ref="showCompleted" checked={showCompleted} onChange={() => {
+                            dispatch(actions.toggleShowCompleted());
+                        }}/>
                         show completed todos
                     </label>
                 </div>
@@ -24,4 +32,11 @@ var TodoSearch = React.createClass({
     }
 });
 
-module.exports = TodoSearch;
+export default connect(
+    (state) => {
+        return {
+            showCompleted: state.showCompleted,
+            searchText: state.searchText
+        }
+    }
+)(TodoSearch);
--- app/tests/componets/TodoSearch.test.jsx
+++ app/tests/componets/TodoSearch.test.jsx
@@ -4,31 +4,38 @@ var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
-var TodoSearch = require('TodoSearch');
+//var TodoSearch = require('TodoSearch');
+import {TodoSearch} from 'TodoSearch';
 
 describe('TodoSearch', () => {
     it('should exist', () => {
         expect(TodoSearch).toExist();
     });
 
-    it('should call onSearch with entered input text', () => {
+    it('should dispatch SET_SEARCH_TEXT when entered input text', () => {
+        var action = {
+            type: 'SET_SEARCH_TEXT',
+            searchText: 'dog'
+        };
         var spy = expect.createSpy();
-        var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>);
+        var todoSearch = TestUtils.renderIntoDocument(<TodoSearch dispatch={spy}/>);
 
         todoSearch.refs.searchText.value = "dog";
         TestUtils.Simulate.change(todoSearch.refs.searchText);
 
-        expect(spy).toHaveBeenCalledWith(false, 'dog');
+        expect(spy).toHaveBeenCalledWith(action);
     });
 
-    it('should call onSearch with proper checked value', () => {
-        var spy = expect.createSpy();
-        var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>);
+    // it('should dispatch TOGGLE_SHOW_COMPLETED when checked value', () => {
+    //     var action = {
+    //         type: 'TOGGLE_SHOW_COMPLETED'
+    //     };
+    //     var spy = expect.createSpy();
+    //     var todoSearch = TestUtils.renderIntoDocument(<TodoSearch dispatch={spy}/>);
 
-        todoSearch.refs.searchText.value = "dog";
-        todoSearch.refs.showCompleted.checked = true;
-        TestUtils.Simulate.change(todoSearch.refs.searchText);
+    //     todoSearch.refs.showCompleted.checked = true;
+    //     TestUtils.Simulate.change(todoSearch.refs.searchText);
 
-        expect(spy).toHaveBeenCalledWith(true, 'dog');
-    });
+    //     expect(spy).toHaveBeenCalledWith(action);
+    // });
 });
